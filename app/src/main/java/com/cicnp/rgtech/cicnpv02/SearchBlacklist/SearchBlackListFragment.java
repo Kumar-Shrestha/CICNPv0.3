515211594a477cff2ee1e2ab5914bf81c0e4726e
Kumar-Shrestha <kmr.shr324@gmail.com>
2016-11-07 20:44:11 +0545

    Search Criteria added

--- app/src/main/java/com/cicnp/rgtech/cicnpv02/SearchBlacklist/SearchBlackListFragment.java
+++ app/src/main/java/com/cicnp/rgtech/cicnpv02/SearchBlacklist/SearchBlackListFragment.java
@@ -7,10 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.cicnp.rgtech.cicnpv02.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +27,7 @@ public class SearchBlackListFragment extends Fragment implements View.OnClickLis
 
     EditText editText_search;
     Button button_search;
+    Spinner spinner_criteria;
 
     public SearchBlackListFragment() {
         // Required empty public constructor
@@ -37,6 +44,21 @@ public class SearchBlackListFragment extends Fragment implements View.OnClickLis
 
         button_search = (Button) view.findViewById(R.id.searchBlacklist_button_search);
         button_search.setOnClickListener(this);
+
+        // Spinner element
+        spinner_criteria = (Spinner) view.findViewById(R.id.searchBlackList_spinner_searchCriteria);
+
+        String[] criteria = getResources().getStringArray(R.array.search_criteria);
+        List<String> countryList = new ArrayList<String>(Arrays.asList(criteria));
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, countryList);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner_criteria.setAdapter(dataAdapter);
 
         return view;
     }
